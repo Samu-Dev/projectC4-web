9ac385ee78ef58b6ed259c41c7b33df1c1c22889
Samu-Dev <samcorredor1@gmail.com>
2022-11-06 20:14:08 -0500

    organización

--- app-projectc4/src/Componentes/PiePagina.js
+++ app-projectc4/src/Componentes/PiePagina.js
@@ -11,10 +11,10 @@ class PiePagina extends Component {
                     <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
                         <div className="col-md-4 d-flex align-items-center">
                             <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-                                <img className="bi" width="100" height="auto" src={process.env.PUBLIC_URL+"./recursos/logo_utp.png"} alt=""></img>
+                                <img className="bi" width="150" height="auto" src={process.env.PUBLIC_URL+"./recursos/logo_utp.png"} alt=""></img>
                             </a>
-                            <span className="mb-3 mb-md-0 text-muted">&copy; 2022 Company, Inc</span>
-                            <span className="mb-3 mb-md-0 text-muted">Nuestro equipo</span>
+                            <span className="mb-3 mb-md-0 text-white">&copy; 2022 Company, Inc</span>
+                            <span className="mb-3 mb-md-0 text-white">Nuestro equipo</span>
                         </div>
                     </footer>
                 </div>
