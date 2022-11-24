01be7089061a75e0183e034c7cee5661b846e846
Samu-Dev <samcorredor1@gmail.com>
2022-11-24 09:19:33 -0500

    ajustes Team

--- app-projectc4/src/Componentes/Team.js
+++ app-projectc4/src/Componentes/Team.js
@@ -6,73 +6,69 @@ class Team extends Component{
     render() {
         return (
             <main>
-            <section id="team" class="team">
-                <div class="container">
-                    <div class="section-title" data-aos="fade-in" data-aos-delay="100">
+            <section id="team" className="team">
+                <div className="container">
+                    <div className="section-title" data-aos="fade-in" data-aos-delay="100">
                         <h2>Equipo 1 Grupo 45</h2>
                         <p></p>
                     </div>
-                    <div class="row">
-                        <div class="col-lg-6 col-md-6">
-                            <div class="member" data-aos="fade-up">
-                                <div class="pic"><img src="" class="img-fluid" alt=""></div>
-                                <div class="member-info">
+                    <div className="row">
+                        <div className="col-lg-6 col-md-6">
+                            <div className="member" data-aos="fade-up">
+                                <div className="pic"><img src="https://github.com/Samu-Dev/projectC4-web/blob/main/IMAGENES/fotos-team/Luisa.png" className="img-fluid" alt=""/></div>
+                                <div className="member-info">
                                 <h4>Luisa Fernanda Londoño</h4>
                                 <span>Gestora de Base De Datos</span>
-                                    <div class="social">
-                                        <a href=""><i class="bi bi-twitter"></i></a>
-                                        <a href=""><i class="bi bi-facebook"></i></a>
-                                        <a href=""><i class="bi bi-instagram"></i></a>
-                                        <a href=""><i class="bi bi-linkedin"></i></a>
+                                    <div className="social">
+                                        <a href=""><i className="bi bi-facebook"></i></a>
+                                        <a href=""><i className="bi bi-instagram"></i></a>
+                                        <a href=""><i className="bi bi-linkedin"></i></a>
                                     </div>
                                 </div>
                             </div>
                         </div>
             
-                        <div class="col-lg-6 col-md-6">
-                            <div class="member" data-aos="fade-up" data-aos-delay="150">
-                                <div class="pic"><img src="" class="img-fluid" alt=""></div>
-                                <div class="member-info">
+                        <div className="col-lg-6 col-md-6">
+                            <div className="member" data-aos="fade-up" data-aos-delay="150">
+                                <div className="pic"><img src="https://github.com/Samu-Dev/projectC4-web/blob/main/IMAGENES/fotos-team/Samuel.png" className="img-fluid" alt=""/></div>
+                                <div className="member-info">
                                     <h4>Samuel Corredor Carrero</h4>
                                     <span>Desarrollador Front-End</span>
-                                    <div class="social">
-                                        <a href=""><i class="bi bi-twitter"></i></a>
-                                        <a href=""><i class="bi bi-facebook"></i></a>
-                                        <a href=""><i class="bi bi-instagram"></i></a>
-                                        <a href=""><i class="bi bi-linkedin"></i></a>
+                                    <div className="social">
+                                        <a href=""><i className="bi bi-facebook"></i></a>
+                                        <a href=""><i className="bi bi-instagram"></i></a>
+                                        <a href=""><i className="bi bi-linkedin"></i></a>
                                     </div>
                                 </div>
                             </div>
                         </div>
             
-                        <div class="col-lg-6 col-md-6">
-                            <div class="member" data-aos="fade-up" data-aos-delay="300">
-                                <div class="pic"><img src="" class="img-fluid" alt=""></div>
-                                <div class="member-info">
+                        <div className="col-lg-6 col-md-6">
+                            <div className="member" data-aos="fade-up" data-aos-delay="300">
+                                <div className="pic"><img src="https://github.com/Samu-Dev/projectC4-web/blob/main/IMAGENES/fotos-team/JorgeAndres.png" className="img-fluid" alt=""/></div>
+                                <div className="member-info">
                                 <h4>Jorge Andrés Hoyos</h4>
                                 <span>Desarrollador Back-End</span>
-                                <div class="social">
-                                    <a href=""><i class="bi bi-twitter"></i></a>
-                                    <a href=""><i class="bi bi-facebook"></i></a>
-                                    <a href=""><i class="bi bi-instagram"></i></a>
-                                    <a href=""><i class="bi bi-linkedin"></i></a>
+                                <div className="social">
+                                    <a href=""><i className="bi bi-facebook"></i></a>
+                                    <a href=""><i className="bi bi-instagram"></i></a>
+                                    <a href=""><i className="bi bi-linkedin"></i></a>
                                 </div>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="col-lg-6 col-md-6">
-                            <div class="member" data-aos="fade-up">
-                                <div class="pic"><img src="" class="img-fluid" alt=""></div>
-                                <div class="member-info">
+                        <div className="col-lg-6 col-md-6">
+                            <div className="member" data-aos="fade-up">
+                                <div className="pic"><img src="https://github.com/Samu-Dev/projectC4-web/blob/main/IMAGENES/fotos-team/JorgeMario.png" className="img-fluid" alt=""/></div>
+                                <div className="member-info">
                                 <h4>Jorge Mario Mendoza</h4>
                                 <span>Tester</span>
-                                <div class="social">
-                                    <a href=""><i class="bi bi-twitter"></i></a>
-                                    <a href=""><i class="bi bi-facebook"></i></a>
-                                    <a href=""><i class="bi bi-instagram"></i></a>
-                                    <a href=""><i class="bi bi-linkedin"></i></a>
-                                </div>
+                                    <div className="social">
+                                        <a href=""><i className="bi bi-facebook"></i></a>
+                                        <a href=""><i className="bi bi-instagram"></i></a>
+                                        <a href=""><i className="bi bi-linkedin"></i></a>
+                                    </div>
                                 </div>
                             </div>
                         </div>            
@@ -83,3 +79,5 @@ class Team extends Component{
         )
     }
 }
+
+export { Team };
